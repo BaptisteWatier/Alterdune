e3033e2e84da79bbb653db5e4d57eb983f5ffbad
Noemie31500 <noemie.schaeffer@edu.devinci.fr>
2026-04-08 17:38:15 +0200

    Update bestiary.h

--- Alterdune/bestiary.h
+++ Alterdune/bestiary.h
@@ -1,1 +1,23 @@
-#pragma once
+#ifndef BESTIARY_H
+#define BESTIARY_H
+
+#include <string>
+#include <vector>
+#include "Monster.h"
+
+using namespace std;
+
+class Bestiary {
+protected:
+    vector<Monster> defeatedMonsters;
+    vector<string> results;
+
+public:
+    Bestiary(vector <Monster> defeatedMonsters, vector <string> results);
+
+    void addEntry(Monster, string);
+    void showBestiary();
+
+};
+
+#endif
