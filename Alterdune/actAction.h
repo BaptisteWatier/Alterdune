019ed58b3ab1fcfc9ec11acec2e6110f6b1d53c0
Noemie31500 <noemie.schaeffer@edu.devinci.fr>
2026-04-08 17:39:35 +0200

    Update actAction.h

--- Alterdune/actAction.h
+++ Alterdune/actAction.h
@@ -1,1 +1,20 @@
-#pragma once
+#ifndef ACTACTION_H
+#define ACTACTION_H
+
+#include <string>
+
+using namespace std;
+
+class ActAction {
+protected:
+    string id;
+    string texte;
+    int mercyImpact;
+
+public:
+    ActAction(string id, string texte, int mercyImpact);
+
+    string getTexte();
+    string getMercyImpact();
+    
+}
