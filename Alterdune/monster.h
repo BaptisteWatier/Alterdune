3dc24e241f64bb9b825eec9bec0e12d8bb8c2d23
Noemie31500 <noemie.schaeffer@edu.devinci.fr>
2026-04-08 17:37:51 +0200

    Update monster.h

--- Alterdune/monster.h
+++ Alterdune/monster.h
@@ -1,1 +1,28 @@
-#pragma once
+#ifndef MONSTER_H
+#define MONSTER_H
+
+#include <string>
+
+using namespace std;
+
+class Monster {
+protected:
+    string monsterCategory; // Classe ?
+    int mercy;
+    int mercyGoal;
+    vector<string> actIds;
+
+public:
+    Monster(string monsterCategory, int mercy, int mercyGoal, vector<string> actIds);
+
+    string getMonsterCategory();
+    int getMercy();
+    int getMercyGoal();
+    vector<string> getActIds();
+
+    void changeMercy(int);
+    bool canBeSpared();
+    void resetMercy();
+};
+
+#endif
